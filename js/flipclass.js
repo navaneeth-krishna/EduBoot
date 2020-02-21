9e15e405ecdec977012166a51f1b8abce716c7e9
navaneeth-krishna <54241636+navaneeth-krishna@users.noreply.github.com>
2020-02-22 00:30:14 +0530

    Flip Class Update

---
+++ js/flipclass.js
@@ -0,0 +1,31 @@
+alert("Hi")
+
+document.getElementById("defaultOpen").click();
+
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = '#ff406e';
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+  elmnt.style.color = '#edf2ff';
+
+
+}
+
+// Get the element with id="defaultOpen" and click on it
